cb2a6921cdb15eb50db1fd0d1762beb877c904ba
Rakesh <kohalirakesh@gmail.com>
2020-06-25 12:10:24 +0300

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  theme: {
+    fontFamily:{
+      gotham:['Gotham Pro', 'sans-serif']
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1590px'
+    },
+    colors:{
+      white:'#FFFFFF',
+      orange: {
+        100:'#FEEDD3',
+        200:'#FE7865',
+        300:'#E6604D',
+        400:'#F55C46'
+      },
+      blue:{
+        100: '#F5F8FC',
+        200: '#EFEDFE',
+        300: '#5BD5E6'
+      },
+      green:{
+        100:'#FFFBF8',
+        200:'#E8FBF7',
+        300:'#5BE6B0'
+      },
+      gray:{
+        100:'#ECECEE',
+        200:'#DFDAFE',
+        300:'#83838B',
+        400:'#F2F2F4'
+       
+      },
+      pink:{
+        100:'#FEDAE8'
+      },
+      black:{
+        100:'#2C2E3F',
+        200:'#141517',
+        300:'#2C2E3F',
+        400:'#1D2539'
+      },
+      yellow:{
+        100:'#FEC365'
+      }
+    },
+    extend: {
+      fontSize:{
+        '8xl': '6rem'
+      },
+      spacing:{
+        '144' : '36rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
